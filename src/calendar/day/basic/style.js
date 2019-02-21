64e1d222dcd16db7295d6e1666dd6841738b245a
Yudi Edri Alviska <yudi.a@weefer.co.id>
2019-02-21 16:44:19 +0700

    change style today

--- src/calendar/day/basic/style.js
+++ src/calendar/day/basic/style.js
@@ -1,13 +1,13 @@
-import {StyleSheet, Platform} from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 import * as defaultStyle from '../../../style';
 
 const STYLESHEET_ID = 'stylesheet.day.basic';
 
-export default function styleConstructor(theme={}) {
-  const appStyle = {...defaultStyle, ...theme};
+export default function styleConstructor(theme = {}) {
+  const appStyle = { ...defaultStyle, ...theme };
   return StyleSheet.create({
     base: {
-      width: 32,
+      width: 35,
       height: 35,
       alignItems: 'center'
     },
@@ -27,10 +27,15 @@ export default function styleConstructor(theme={}) {
       borderRadius: 16
     },
     today: {
-      backgroundColor: appStyle.todayBackgroundColor
+      backgroundColor: '#dcdcdc',
+      borderRadius: 20,
     },
     todayText: {
-      color: appStyle.todayTextColor
+      marginTop: Platform.OS === 'android' ? 4 : 6,
+      fontSize: appStyle.textDayFontSize,
+      fontFamily: appStyle.textDayFontFamily,
+      fontWeight: '300',
+      color: appStyle.dayTextColor,
     },
     selectedText: {
       color: appStyle.selectedDayTextColor
